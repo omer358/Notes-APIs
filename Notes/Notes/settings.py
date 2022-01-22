cf6357e44d0a8a82f9e30d57522cc851035cf6ae
omer358 <omermaki358@gmail.com>
2022-01-22 20:36:11 +0200

    regenerate SECRET_KEY and hide it

--- Notes/Notes/settings.py
+++ Notes/Notes/settings.py
@@ -12,20 +12,23 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
 from django_on_heroku import settings as django_on_heroku_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from dotenv import load_dotenv
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!)ph3a9n@m-3=4j27u@wt!zjuqfi#r%3t^w=!s=kz-)m+we1+u"
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
