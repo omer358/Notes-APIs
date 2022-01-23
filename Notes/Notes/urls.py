48d49935e8563e232a23e2dc067dda1c61a7beb5
omer358 <omermaki358@gmail.com>
2022-01-23 17:18:50 +0200

    Update the main url for documentations

--- Notes/Notes/urls.py
+++ Notes/Notes/urls.py
@@ -34,9 +34,8 @@ urlpatterns = [
 ]
 urlpatterns += [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    path('', include(router.urls)),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('logout/', Logout.as_view(), name='logout'),
     path('register/', Register.as_view(), name='register'),
