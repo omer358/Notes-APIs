092a44ed7d283118d0a9a2420c36ded0d414b00b
omer358 <omermaki358@gmail.com>
2023-10-10 02:40:33 +0200

    edit endpoints

--- Notes/Notes/urls.py
+++ Notes/Notes/urls.py
@@ -32,11 +32,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 urlpatterns += [
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema', SpectacularAPIView.as_view(), name='schema'),
     path('docs', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/schema/redoc', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls)),
-    path('register/', Register.as_view(), name='register'),
+    path('register', Register.as_view(), name='register'),
 ]
